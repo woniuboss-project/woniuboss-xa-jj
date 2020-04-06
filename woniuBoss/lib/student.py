f469344f87a3d2eedb33feb8e28f6e1468c03d8b
Zilong <zilongchina@gmail.com>
2020-04-06 23:05:44 +0800

    学员管理模块

--- woniuBoss/lib/student.py
+++ woniuBoss/lib/student.py
@@ -1,5 +1,17 @@
+from woniuBoss.tools.service import Service
 from woniuBoss.tools.utility import Utility
 
 
 class Student:
     pass
+
+
+class StudentApi:
+    def __init__(self, conf_data):
+        self.session = Service.get_session(conf_data)
+
+    def get_request(self, data):
+        return self.session.get(data['url'])
+
+    def post_request(self, post_data):
+        return self.session.post(post_data['url'], post_data['data'])
